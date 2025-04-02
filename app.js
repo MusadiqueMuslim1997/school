e8c3cfead54d0dc998db1b28c43c9c882331c3d4
MusadiqueMuslim1997 <musadique1997@gmail.com>
2025-04-02 16:30:39 +0500

    add sweet alert

--- app.js
+++ app.js
@@ -10,3 +10,30 @@ document.getElementById("show").addEventListener("change", function() {
       passwordField.type = "password";
     }
   });
+
+  
+  function validInput(){
+    let passwordField = document.getElementById("password").value;
+    let email = document.getElementById("inp").value;
+    if(passwordField ===""||email===""){
+      Swal.fire({
+        icon: "error",
+        text: "Please enter these input field!",
+      });
+    }
+    else if(passwordField.length<6){
+      Swal.fire({
+        icon: "error",
+        text: "Please enter atleast 6 digits password!",
+      });
+
+    }
+    else{
+      Swal.fire({
+        title: "congratulation!",
+        text: "You have login successfully!",
+        icon: "success"
+      });
+    }
+
+  }
